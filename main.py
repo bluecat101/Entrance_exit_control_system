da4a9c02101518692a1de4e49fab56a87ad87ae6
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-12-26 12:49:00 +0900

    spreadsheetに移行した影響を反映

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template, redirect, request, jsonify
 import os
-import datetime
 import config
+import spreadsheet
 
-TIME_FILE = "in_out_name.txt"
-TIME_LOG_FILE = "in_out_time.log"
-def write_log(user, action):
-  with open(TIME_LOG_FILE, 'a') as f:
-    dt= datetime.datetime.now()
-    str_dt = dt.strftime('%Y-%m-%d %H:%M:%S')
-    f.write(f"{action}:({user}):{str_dt} \n")
   
 app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route('/')
@@ -18,49 +11,34 @@ def main():
 
 @app.route('/arrive_at_work', methods=['POST'])
 def post_arrive_at_work():
-  if "user_out" not in request.form:
+  if "user_out" not in request.form: # outの状態のuserが選択されるためuser_outとなる
     return redirect('/')
   user = request.form['user_out']
-  with open(TIME_FILE, 'r') as f:
-    name_list = [s.rstrip() for s in f.readlines()]
-  if user not in name_list:
-    with open(TIME_FILE, 'a') as f:
-      f.write(user+'\n')
-  write_log(user, "arrive_at_work")
+  spreadsheet.in_out_user(user, "in")
   return redirect('/')
 
 
 @app.route('/clock_out', methods=['POST'])
 def post_clock_out():
-  if "user_in" not in request.form:
+  if "user_in" not in request.form: # inの状態のuserが選択されるためuser_inとなる
     return redirect('/')
   user = request.form['user_in']
-  with open(TIME_FILE, 'r') as f:
-    name_list = [s.rstrip() for s in f.readlines()]
-  if user in name_list:
-    name_list.remove(user)
-  with open(TIME_FILE, 'w') as f:
-    for name in name_list:
-      f.write(name + '\n')
-  write_log(user, "clock_out")
+  spreadsheet.in_out_user(user, "out")
   return redirect('/')
 
+# userの状態を取得する
 @app.route('/get_in_out_name')
 def get_in_out_name():
-  file_path = os.path.join(os.getcwd(), TIME_FILE)
-  if os.path.exists(file_path):
-    with open(TIME_FILE, 'r') as f:
-      name_list = [s.rstrip() for s in f.readlines()]
-    return jsonify({'name': name_list})
-  else:
-      return jsonify({'error': 'File not found'}), 404
+  users_in, users_out = spreadsheet.get_user_status()
+  return jsonify({'users_in': users_in, 'users_out': users_out})
 
+# userの名前一覧を取得する
 @app.route('/get_name_list')
 def get_name_list():
   name_list = config.get_name_list()
   if len(name_list) != 0:
     return jsonify({'name_list': name_list})
   else:
-      return jsonify({'error': 'File not found'}), 404
+    return jsonify({'error': 'File not found'}), 404
 
 app.run(port=8000, debug=True)  
--- spreadsheet.py
+++ spreadsheet.py
@@ -4,16 +4,21 @@ from oauth2client.service_account import ServiceAccountCredentials
 import datetime
 
 
-
 # 認証情報の設定
 GOOGLE_APPLICATION_CREDENTIALS_PATH = config.GOOGLE_APPLICATION_CREDENTIALS_PATH
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_APPLICATION_CREDENTIALS_PATH, scope)
 # クライアントの作成
 client = gspread.authorize(creds)
+
 # スプレッドシートの取得
 SPREADSHEET_NAME = config.SPREADSHEET_NAME
 spreadsheet = client.open(SPREADSHEET_NAME)
+# シートの取得
+IN_USER_COLUMN = 1
+OUT_USER_COLUMN = 3
+OCCUPANCY_STATUS_SHEET_NAME = "在室状況"
+OCCUPANCY_STATUS_WORKSHEET = spreadsheet.worksheet(OCCUPANCY_STATUS_SHEET_NAME)
 
 # ログを書く用
 def write_log(name, status):
@@ -24,11 +29,7 @@ def write_log(name, status):
   log_worksheet.append_row([str_dt, name, status])
 
 
-def in_out_user(name, status):
-  # 設定
-  IN_USER_COLUMN = "A2:A100"
-  OUT_USER_COLUMN = "C2:C100"
-  
+def in_out_user(name, status):  
   if status == "in": # inになったのでinに書き込み、outから取り除く
     column_to_add = IN_USER_COLUMN
     column_name_to_add = "A"
@@ -43,19 +44,25 @@ def in_out_user(name, status):
   else:
     return
   
-  occupancy_status_sheet_name = "在室状況"
-  occupancy_status_worksheet = spreadsheet.worksheet(occupancy_status_sheet_name)
   # 名前を削除
-  users_to_delete = occupancy_status_worksheet.get(column_to_delete)[0]
+  users_to_delete = OCCUPANCY_STATUS_WORKSHEET.col_values(column_to_delete)[1:]
+  if name not in users_to_delete:
+    return False
+  OCCUPANCY_STATUS_WORKSHEET.update(f'{column_name_to_delete}{2}:{column_name_to_delete}{len(users_to_delete)+1}', [[""] for user in users_to_delete]) # 初期化
   users_to_delete.remove(name)
-  occupancy_status_worksheet.update(column_to_delete, [[users_to_delete]])
+  OCCUPANCY_STATUS_WORKSHEET.update(f'{column_name_to_delete}{2}:{column_name_to_delete}{len(users_to_delete)+1}', [[user] for user in users_to_delete])
   
   # 名前を追加
-  num_in_user = len(occupancy_status_worksheet.get(column_to_add)[0])
-  out_users.remove(name)
-  occupancy_status_worksheet.update_acell(f'{column_name_to_add}{num_in_user+2}', name)
+  num_in_user = len(OCCUPANCY_STATUS_WORKSHEET.col_values(column_to_add)[1:])
+  OCCUPANCY_STATUS_WORKSHEET.update_acell(f'{column_name_to_add}{num_in_user+2}', name)
 
   if status == "in":
     write_log(name, "不在→在室")
   elif status == "out":
     write_log(name, "在室→不在")
+
+# userの状態を全て取得
+def get_user_status():
+  users_in = OCCUPANCY_STATUS_WORKSHEET.col_values(IN_USER_COLUMN)[1:]
+  users_out = OCCUPANCY_STATUS_WORKSHEET.col_values(OUT_USER_COLUMN)[1:]
+  return users_in, users_out
