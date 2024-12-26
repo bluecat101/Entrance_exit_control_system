8d3c956765e9296c81950475c5c546d3623e9ea2
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2024-12-26 21:10:08 +0900

    スプレッドシートの呼び出しを一度飲みにして高速化

--- spreadsheet.py
+++ spreadsheet.py
@@ -15,6 +15,8 @@ client = gspread.authorize(creds)
 SPREADSHEET_NAME = config.SPREADSHEET_NAME
 spreadsheet = client.open(SPREADSHEET_NAME)
 # シートの取得
+MAX_LEN = 19
+ALL_USER_COLUMN = f"A2:C{MAX_LEN+1}"
 IN_USER_COLUMN = 1
 OUT_USER_COLUMN = 3
 OCCUPANCY_STATUS_SHEET_NAME = "在室状況"
@@ -30,32 +32,31 @@ def write_log(name, status):
 
 
 def in_out_user(name, status):  
-  if status == "in": # inになったのでinに書き込み、outから取り除く
-    column_to_add = IN_USER_COLUMN
-    column_name_to_add = "A"
-    column_to_delete = OUT_USER_COLUMN
-    column_name_to_delete = "C"
-    
-  elif status == "out": # outになったのでoutに書き込み、inから取り除く
-    column_to_add = OUT_USER_COLUMN
-    column_name_to_add = "C"
-    column_to_delete = IN_USER_COLUMN
-    column_name_to_delete = "A"
-  else:
-    return
-  
-  # 名前を削除
-  users_to_delete = OCCUPANCY_STATUS_WORKSHEET.col_values(column_to_delete)[1:]
+  # 名前の取得
+  users_in, users_out = get_user_status()
+  if status == "in":
+    users_to_add = users_in
+    users_to_delete = users_out
+  elif status == "out":
+    users_to_add = users_out
+    users_to_delete = users_in
+  # 名前を削除,追加
   if name not in users_to_delete:
     return False
-  OCCUPANCY_STATUS_WORKSHEET.update(f'{column_name_to_delete}{2}:{column_name_to_delete}{len(users_to_delete)+1}', [[""] for user in users_to_delete]) # 初期化
   users_to_delete.remove(name)
-  OCCUPANCY_STATUS_WORKSHEET.update(f'{column_name_to_delete}{2}:{column_name_to_delete}{len(users_to_delete)+1}', [[user] for user in users_to_delete])
+  users_to_add += [name] 
+  # ダミーデータの追加
+  users_to_add += [""] * (MAX_LEN - len(users_to_add))
+  users_to_delete +=  [""] * (MAX_LEN - len(users_to_delete))
+  # 1度のスプレッドシートへの通信で済ませるようにまとめる
+  users_to_write = []
+  for i in range(MAX_LEN):
+    if status == "in":
+      users_to_write.append([users_to_add[i], "", users_to_delete[i]])
+    elif status == "out":
+      users_to_write.append([users_to_delete[i],"",users_to_add[i]])
+  OCCUPANCY_STATUS_WORKSHEET.update(ALL_USER_COLUMN, users_to_write)
   
-  # 名前を追加
-  num_in_user = len(OCCUPANCY_STATUS_WORKSHEET.col_values(column_to_add)[1:])
-  OCCUPANCY_STATUS_WORKSHEET.update_acell(f'{column_name_to_add}{num_in_user+2}', name)
-
   if status == "in":
     write_log(name, "不在→在室")
   elif status == "out":
@@ -63,6 +64,15 @@ def in_out_user(name, status):
 
 # userの状態を全て取得
 def get_user_status():
-  users_in = OCCUPANCY_STATUS_WORKSHEET.col_values(IN_USER_COLUMN)[1:]
-  users_out = OCCUPANCY_STATUS_WORKSHEET.col_values(OUT_USER_COLUMN)[1:]
+  users = OCCUPANCY_STATUS_WORKSHEET.get(ALL_USER_COLUMN)
+  users_in = []
+  users_out = []
+  for user in users:
+    if len(user) == 3:
+      if user[0] != "":
+        users_in.append(user[0])
+      if user[2] != "":
+        users_out.append(user[2])
+    if len(user) < 3:
+      users_in.append(user[0])
   return users_in, users_out
